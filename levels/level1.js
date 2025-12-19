1e3fee83bb38c34e2ca73debb031f414fc6f49d4
serhat özcakir <serhatozcakir35@gmail.com>
2025-12-19 16:18:47 +0100

    new Coins() and new Bottles() added

--- levels/level1.js
+++ levels/level1.js
@@ -1,3 +1,5 @@
+
+
 const level1 = new Level([
     new Chicken(),
     new Chicken(),
@@ -8,8 +10,6 @@ const level1 = new Level([
     new Clouds()
 ],
 [
-
-    
     new BackgroundObject('image/5_background/layers/air.png', -719),
     new BackgroundObject('image/5_background/layers/3_third_layer/1.png', -719),
     new BackgroundObject('image/5_background/layers/2_second_layer/1.png', -719),
@@ -34,7 +34,30 @@ const level1 = new Level([
     new BackgroundObject('image/5_background/layers/3_third_layer/1.png', 719*3),
     new BackgroundObject('image/5_background/layers/2_second_layer/1.png', 719*3),
     new BackgroundObject('image/5_background/layers/1_first_layer/1.png', 719*3),
-
-
-]
+], 
+[
+    new Coins(),
+    new Coins(),
+    new Coins(),
+    new Coins(),
+    new Coins(),
+    new Coins(),
+    new Coins(),
+    new Coins(),
+    new Coins(),
+    new Coins(),
+    new Coins()
+],
+ [
+        new Bottles(300, 380),
+        new Bottles(400, 380),
+        new Bottles(450, 380),
+        new Bottles(600, 380),
+        new Bottles(680, 380),
+        new Bottles(750, 380),
+        new Bottles(1000, 380),
+        new Bottles(1200, 380),
+        new Bottles(1350, 380),
+        new Bottles(1500, 380)
+    ]
 )
